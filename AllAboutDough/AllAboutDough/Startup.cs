334e16437647c9a127cdd3c527136d94530dd434
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-09-03 19:59:46 +0200

    feat(AllAboutDough):add dependency injection

---
+++ AllAboutDough/AllAboutDough/Startup.cs
@@ -0,0 +1,31 @@
+﻿using AllAboutDough.Repositories;
+using AllAboutDough.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AllAboutDough
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string connectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=AllAboutDough;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            services.AddDbContext<OrderDbContext>(options => options.UseSqlServer(connectionString));
+            services.AddTransient<OrderDbContext>();
+            services.AddTransient<OrderRepository>();
+            services.AddTransient<OrderService>();
+        }
+    }
+}

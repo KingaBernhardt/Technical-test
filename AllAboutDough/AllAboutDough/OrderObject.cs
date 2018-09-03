642aa597f4541771776dcc2cf9ac2b929ddb6839
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-09-03 19:54:03 +0200

    feat(AllAboutDough):create object from Json

---
+++ AllAboutDough/AllAboutDough/OrderObject.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AllAboutDough
+{
+    public class OrderTransferObject
+    {
+        public List<Order> Orders { get; set; }
+    }
+
+    public class Order
+    {
+        public string OrderDate { get; set; }
+        public List<string> Toppings { get; set; }
+    }
+}

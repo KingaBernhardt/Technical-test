37c61dd1acd7e38c9696f1a2f42f55583915d93d
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-09-03 19:56:24 +0200

    feat(AllAboutDough):create orders model

---
+++ AllAboutDough/AllAboutDough/Models/Orders.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AllAboutDough
+{
+    public class Orders
+    {
+        private Guid orderId;
+        [Key]
+        public Guid OrderId
+        {
+            get; set;
+        }
+        public DateTime OrderDate { get; set; }
+        public string Topping { get; set; }
+        public bool IsToppingVegetarian { get; set; }
+    }
+}

28300a129b627e906d66d5149b9d866208160eeb
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-09-03 19:58:35 +0200

    feat(AllAboutDough)start working in repos

---
+++ AllAboutDough/AllAboutDough/Repositories/OrderDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace AllAboutDough.Repositories
+{
+    public class OrderDbContext : DbContext
+    {
+        public DbSet<Orders> Orders { get; set; }
+
+        public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options)
+        {
+
+        }
+    }
+
+    public class OrderContextDbFactory : IDesignTimeDbContextFactory<OrderDbContext>
+    {
+         OrderDbContext IDesignTimeDbContextFactory<OrderDbContext>.CreateDbContext(string[] args)
+         {
+            var optionsBuilder = new DbContextOptionsBuilder<OrderDbContext>();
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\ProjectsV13;Initial Catalog=AllAboutDough;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+            return new OrderDbContext(optionsBuilder.Options);
+         }
+    }
+}
---
+++ AllAboutDough/AllAboutDough/Repositories/OrderRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AllAboutDough.Repositories
+{
+    public class OrderRepository
+    {
+        private OrderDbContext orderDbContext;
+
+        public void OrderDbContext(OrderDbContext orderDbContext)
+        {
+            this.orderDbContext = orderDbContext;
+        }
+
+        public void Create(Orders orders)
+        {
+            orderDbContext.Orders.Add(orders);
+            orderDbContext.SaveChanges();
+        }
+
+        public void Delete(Orders orders)
+        {
+            orderDbContext.Orders.Remove(orders);
+            orderDbContext.SaveChanges();
+        }
+
+        public List<Orders> Read()
+        {
+            return orderDbContext.Orders.ToList();
+        }
+
+        public void Update(Orders matrix)
+        {
+            orderDbContext.Orders.Update(matrix);
+            orderDbContext.SaveChanges();
+        }
+    }
+}

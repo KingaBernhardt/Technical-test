e54c0ffdc94f179edc53e8d7c09b4b1b8a66778f
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-09-03 19:57:25 +0200

    feat(AllAboutDough):create database from model

---
+++ AllAboutDough/AllAboutDough/Migrations/20180902111804_InitialCreateDb.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AllAboutDough.Migrations
+{
+    public partial class InitialCreateDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    OrderId = table.Column<Guid>(nullable: false),
+                    OrderDate = table.Column<DateTime>(nullable: false),
+                    Topping = table.Column<string>(nullable: true),
+                    IsToppingVegetarian = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
